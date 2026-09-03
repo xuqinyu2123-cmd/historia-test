3196dec6a8695df729828de56dd4b234839d9cfe
xuqinyu2123-cmd <xuqinyu2123@gmail.com>
2026-09-03 20:07:28 +0800

    update to HISTORIA V5 journal edition

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="historia-v4-20260903";
+const CACHE="historia-v5-20260903";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./apple-touch-icon.png"];
 self.addEventListener("install",e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
